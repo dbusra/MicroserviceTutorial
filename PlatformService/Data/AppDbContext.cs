5eb41e90d3e84ea6c246a7c6794ce36a43eef925
busra <doganbusra94@gmail.com>
2021-11-28 23:19:44 +0300

    dbcontext

---
+++ PlatformService/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using PlatformService.Models;
+
+namespace PlatformService.Data
+{
+    public class AppDbContext : DbContext
+    {
+        /*note: with 'base' we can use functionality of parent class, in this case DbContext*/
+        public AppDbContext(DbContextOptions<AppDbContext> opt) : base(opt) 
+        {
+            
+        }
+
+        public DbSet<Platform> Platforms { get; set; } // represents db 'table'
+
+    }
+}
---
+++ README.txt
@@ -0,0 +1,9 @@
+WE ARE USING 'IN-MEMORY' DATABASE FOR NOW,
+	IN DEVELOPMENT ENVIRONMENT.
+WHEN WE MOVE ON TO PRODUCTION ENVIRONMENT
+	WE ARE GONNA USE 'SQL SERVER' 'KUBERNETES'
+
+i) model is created
+ii) dbcontext (representation of db) is created
+iii) dbset is represantation of 'table' 'entity'
+iv) added dbcontext to startup 'dependency injection?'
