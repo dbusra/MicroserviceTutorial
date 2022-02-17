2c21a01336e468708cd59d7d1208cf387271be3d
busra <doganbusra94@gmail.com>
2022-02-17 23:06:58 +0300

    data layer(models) for command service

---
+++ CommandsService/Models/Command.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace CommandsService.Models
+{
+    public class Command
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string HowTo { get; set; } // how to do a particular activity
+
+        [Required]
+        public string CommandLine { get; set; }
+
+        [Required]
+        public int PlatformId { get; set; } // foreign key for platform id within platform db
+        public Platform Platform { get; set; }
+    }
+}
---
+++ CommandsService/Models/Platform.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace CommandsService.Models
+{
+    public class Platform
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public int ExternalId { get; set; } // primary key of the platform (from platform service)
+        
+        [Required]
+        public string Name { get; set; }
+        public ICollection<Command> Commands { get; set; } = new List<Command>();
+
+    }
+}
